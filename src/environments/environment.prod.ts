8b4a2501056f8112b381724abc8e6f8dfba4f54b
Chris <christophe.sophy.ext@cgi.com>
2026-01-27 20:25:27 +0100

    chore: add cgi auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   msalConfig: {
     auth: {
-      clientId: '2da3ef99-4acb-4a7a-a84b-4f85904bdf65',
-      authority: 'https://login.microsoftonline.com/b037a886-4b8f-4119-a8a9-1bdce833249a',
+      clientId: '841f0375-afb3-41df-86cd-c7c722933b0a',
+      authority: 'https://login.microsoftonline.com/b9fec68c-c92d-461e-9a97-3d03a0f18b82',
       redirectUri: '/',
       postLogoutRedirectUri: '/',
     },
